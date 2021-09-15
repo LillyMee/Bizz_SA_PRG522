765141b4ab0220e7197b4d13b550e21221aefd51
ldhli <ldhli@LILLY-NB>
2021-09-15 16:15:33 +0200

    Add project files.

---
+++ PRG522_SA_2021/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRG522_SA_2021.Startup))]
+namespace PRG522_SA_2021
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
